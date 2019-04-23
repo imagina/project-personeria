7509bbcc342f3c13cc90c49061022d82da82c5f3
Yeison Tapia <yftapial@ut.edu.co>
2019-04-23 09:16:29 -0500

    - add float btn component

--- src/components/app/commons/chatbtn.vue
+++ src/components/app/commons/chatbtn.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>My component</div>
+  <q-page-sticky
+    position="bottom-right" 
+    :offset="[18, 18]">
+    <q-btn
+      round
+      color="primary"
+      icon="chat_bubble_outline"/>
+  </q-page-sticky>
 </template>
 
 <script>
-export default {
-  // name: 'ComponentName',
-  data () {
-    return {}
+  export default {
+    data () {
+      return {}
+    }
   }
-}
 </script>
